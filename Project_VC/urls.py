1da4400acf7ea717890d9246921ad81e4a442bee
loyogoy-g <45865514+loyogoy-g@users.noreply.github.com>
2020-11-20 08:32:22 +0800

    initial commit

--- Project_VC/urls.py
+++ Project_VC/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Gilbert Loyogoy"
+admin.site.site_title = "Grades Portal by Gilbert Loyogoy"
+admin.site.index_title = "Welcome to Grades Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
