2def4945a42af109e5cfac1c359f8756d74ac7ac
loyogoy-g <45865514+loyogoy-g@users.noreply.github.com>
2020-11-20 13:31:16 +0800

    new

--- Project_VC/urls.py
+++ Project_VC/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 admin.site.site_header = "Gilbert Loyogoy"
 admin.site.site_title = "Grades Portal by Gilbert Loyogoy"
 admin.site.index_title = "Welcome to Grades Portal"
@@ -23,4 +27,4 @@ admin.site.index_title = "Welcome to Grades Portal"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
