bc67ade1600a02bf42ec842a3311435545e19007
loyogoy-g <45865514+loyogoy-g@users.noreply.github.com>
2020-11-23 11:11:11 +0800

    Change Databases

--- Project_VC/settings.py
+++ Project_VC/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'Project_VC.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'virtualcard',
+        'USER': 'gilbert',
+        'PASSWORD': 'Qwerty123@',
+        'HOST': '139.59.238.116',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
